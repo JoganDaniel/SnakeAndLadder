5bc56a13a111c8b4eba90fe7b4867d9007ae7589
Jogan Daniel A <jogan.d@tvsnext.com>
2023-07-13 11:40:54 +0530

    [jogan] Add : Folder structure created

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder program");
+        }
+    }
+}
