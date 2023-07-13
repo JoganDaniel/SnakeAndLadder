7b05ab5fe7330b0c87c9df9d3fb0a87de52c939c
Jogan Daniel A <jogan.d@tvsnext.com>
2023-07-13 12:07:47 +0530

    [jogan] Add : Die roll options implemented

--- SnakeAndLadder/SnakeAndLadders.cs
+++ SnakeAndLadder/SnakeAndLadders.cs
@@ -9,18 +9,47 @@ namespace SnakeAndLadder
     public class SnakeAndLadders
     {
         const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION = 100, STARTING_POSITION = 0;
-     
+
         int position = 0;
         int count = 0;
 
-        
+
         Random random = new Random();
         public int DieRoll()
         {
             int diePosition = random.Next(1, 7);
             Console.WriteLine("Position is " + this.position);
-            count++;
             return diePosition;
+        }
+
+        public void Game()
+        {
+
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case NO_PLAY:
+                        break;
+                    case LADDER:
+                        int dieRoll = DieRoll();
+                        if (this.position + dieRoll < WINNING_POSITION)
+                        {
+                            this.position += dieRoll;
+                        }
+                        break;
+                    case SNAKE:
+                        dieRoll = DieRoll();
+                        if (this.position - dieRoll < STARTING_POSITION)
+                        {
+                            this.position = 0;
+                        }
+                        else
+                        {
+                            this.position -= dieRoll;
+                        }
+                        break;
+                }
+            
         }
     }
 }
